3185369653a2d2e35653efb4a8c9182b99d08ef5
Notjessand <jessennorom@gmail.com>
2025-07-06 18:53:57 +0100

    x

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
+// require("dotenv").config();
+// console.log("DB URL:", process.env.DATABASE_URL);
+
+// const express = require("express");
+// const cors = require("cors");
+// const authRoutes = require("./routes/auth");
+// const ticketRoutes = require("./routes/tickets");
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api", authRoutes);
+// app.use("/api", ticketRoutes);
+// app.use("/api", require("./routes/tickets"));
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 require("dotenv").config();
 console.log("DB URL:", process.env.DATABASE_URL);
 
 const express = require("express");
 const cors = require("cors");
-const authRoutes = require("./routes/auth");
-const ticketRoutes = require("./routes/tickets");
+const authRoutes = require("/routes/auth");
+const ticketRoutes = require("/routes/tickets");
 
 const app = express();
 app.use(cors());
